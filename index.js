18cbef072375a893b8a99f0a2a36e39990bd9d2b
kit4699 <nito2010apap@gmail.com>
2024-08-22 05:11:42 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Playing","!Moding Kiyos Server!"];
 
 
 let currentIndex = 0;
